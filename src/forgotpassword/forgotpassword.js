1c33fa09bb52396ff24db7905c020e1908df77c9
Ria Ratna Sari <rrsari@Rias-MacBook-Pro.local>
2019-03-31 21:53:25 +0200

    add login

--- src/forgotpassword/forgotpassword.js
+++ src/forgotpassword/forgotpassword.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import './forgotpassword.css';
 
-const Forgot = props =>{
-    return(
-        <React.Fragment>
-            <h1>Forgot Password</h1>
-            <div className = 'container'>
-            <h2>   This is container</h2>
-            </div>
-        </React.Fragment>
-    )
-}
+const Forgot = props => {
+  return (
+    <React.Fragment>
+        <div className='forgot-container'>
+      <h2>Forgot Password</h2>
+      <div className="container">
+        <div>
+          <label for="exampleInputEmail1">Email address</label>
+          <br />
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Email"
+          />
+        </div>
+        <br/>
+        <button type="submit" className="btn btn-warning">Reset</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Forgot;
